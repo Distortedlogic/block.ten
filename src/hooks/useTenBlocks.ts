c99f8743e3e17c1c01dbf979761ea851439b6bec
distortedlogic <54854447+Distortedlogic@users.noreply.github.com>
2021-01-02 10:26:08 -0500

    make more easily readable

--- src/hooks/useTenBlocks.ts
+++ src/hooks/useTenBlocks.ts
@@ -6,6 +6,16 @@ import { useStoreActions, useStoreState } from "../store/store";
 
 const rpc = new JsonRpc("https://eos.greymass.com/");
 
+const getBlockCustom = async (topBlockNum: number, idx: number) => {
+  try {
+    return await rpc.get_block(topBlockNum - idx);
+  } catch (e) {
+    console.log("\nCaught exception: " + e);
+    if (e instanceof RpcError) console.log(JSON.stringify(e.json, null, 2));
+    return {} as GetBlockResult;
+  }
+};
+
 export const useTenBlocks = (): GetBlockResult[] | undefined => {
   const [tenBlocks, setTenBlocks] = useState<GetBlockResult[]>([]);
   const {
@@ -18,23 +28,11 @@ export const useTenBlocks = (): GetBlockResult[] | undefined => {
     async (isMounted) => {
       if (!isMounted() || !refreshDep) return;
       const topBlockNum = (await rpc.get_info()).head_block_num;
+      const blocks = Array.from(Array(13).keys()).map(
+        async (idx) => await getBlockCustom(topBlockNum, idx)
+      );
       setTenBlocks(
-        (
-          await Promise.all(
-            Array.from(Array(13).keys()).map(async (idx) => {
-              try {
-                return await rpc.get_block(topBlockNum - idx);
-              } catch (e) {
-                console.log("\nCaught exception: " + e);
-                if (e instanceof RpcError)
-                  console.log(JSON.stringify(e.json, null, 2));
-                return {} as GetBlockResult;
-              }
-            })
-          )
-        )
-          .filter((block) => block.id)
-          .slice(0, 10)
+        (await Promise.all(blocks)).filter((block) => block.id).slice(0, 10)
       );
       triggerRefresh(false);
     },
